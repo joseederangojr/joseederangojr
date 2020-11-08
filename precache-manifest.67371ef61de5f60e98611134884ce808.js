644509a34670ea341108425d4a5fc8f0746e1566
Jose Ederango Jr <jxdrng@gmail.com>
2020-11-08 09:48:22 +0800

    Updates

---
+++ precache-manifest.67371ef61de5f60e98611134884ce808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b655ff0be9b6ee208b99424c587a52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9af4f3d661479e297798",
+    "url": "/static/css/2.4c4eb0a5.chunk.css"
+  },
+  {
+    "revision": "9af4f3d661479e297798",
+    "url": "/static/js/2.841c6167.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.841c6167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c1e4bd760856ea1a99",
+    "url": "/static/js/main.d024eb62.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
