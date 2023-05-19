c27ee87bcd994974cbffe8442602c7104b8b8bc4
Jose Ederango Jr <joseederangojr@gmail.com>
2023-05-19 19:21:55 +0800

    Updates

---
+++ precache-manifest.5278b9704f20d1cdca83f8ab2f74499c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79dead855675a61e4c4a7d9abcbd191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28e1e1b3fc07c0a0b889",
+    "url": "/static/css/2.99e0b550.chunk.css"
+  },
+  {
+    "revision": "28e1e1b3fc07c0a0b889",
+    "url": "/static/js/2.aa3df49d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.aa3df49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31352ff6023253ee2a40",
+    "url": "/static/js/main.0b8a5afb.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
